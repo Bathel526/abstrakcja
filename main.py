562672e0ee7e5fa013b73069144eb91162872460
Bartlomiej <bartlomiej1998.03@gmail.com>
2023-04-25 00:05:30 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ class KlasaAbstrakcyjna(ABC):
     def __init__(self):
         raise Exception('Nie mozna wywolywac klasy abstrakcyjnej')
 
-    @abstractmethod   ## taki ala komentarz ze jest to metoda abstakcyjna
+    @abstractmethod   ## taki dekoratkor ze jest to metoda abstakcyjna
     def method_1(self):
         raise NotImplementedError('musisz zaimplementowac ta metode bo bedzie blad')
 
     @abstractmethod
     def method_2(self):
-        pass # brak implementacji nie zglosi takiego leganckego komunikatu jak na gorze
+        pass # brak implementacji nie zglosi takiego eleganckego komunikatu jak na gorze
 
 class Test(KlasaAbstrakcyjna):
     def __init__(self, name):
@@ -20,13 +20,18 @@ class Test(KlasaAbstrakcyjna):
     #   print(self.name)
 
     def method_2(self):
-        print('jem kanapke')
+        print('Welcome')
 
     def test_function(self):
         print('Just checking')
 
-
-#test1 = MetodaAbstrakcyjna()
+test1 = KlasaAbstrakcyjna()
 
 test2 = Test('Bart')
 test2.method_1()
+
+"""
+brak implementacji method_1 - wywali blad
+w klasie dziedziczacej po klasie abstrakcyjnej musza byc zaimplementowane
+wszystkie metody
+"""
